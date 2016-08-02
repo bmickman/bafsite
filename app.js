f4c2853f5a33065c7cdb1eee89e12a0d73c638c2
bmickman <brandon.mickman@gmail.com>
2016-08-02 01:42:43 -0400

    site with old code

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 		angular
 			.module('app',['ui.router','ui.bootstrap','ngAnimate'
-		
-			/*,'uiGmapgoogle-maps'*/
 			])
 			.controller('headerController',function($scope){
 				$scope.isCollapsed=true;
@@ -20,20 +18,24 @@
 						url: '/',
 						templateUrl: 'assets/templates/directory.html',
 						controller: 'homeController'
-					})
+					})/*
 					.state('about',{
 						url: '/',
-						templateUrl: 'assets/templates/about.html'
-						/*controller: 'aboutController'*/
+						templateUrl: 'assets/templates/about.html',
+						controller: 'homeController'
 					})
 					.state('registration',{
 						url: '/',
 						templateUrl:'assets/templates/registration.html'
-						/*controller: 'assets/controllers/registrationController'*/
 					})
 					.state('contact',{
 						url: '/',
 						templateUrl: 'assets/templates/contact.html',
+						controller: 'homeController'
+					})*/
+					.state('sponsors',{
+						url: '/',
+						templateUrl: 'assets/templates/sponsors.html',
 						controller: 'homeController'
 					});
 				
